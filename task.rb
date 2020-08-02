5f9c98177f8aab4eb7786a4309ffe9f4a45068a7
domi10momo <35606852+domi10momo@users.noreply.github.com>
2020-08-02 16:12:34 +0900

    q8破壊的メソッドに修正

--- task.rb
+++ task.rb
@@ -70,7 +70,7 @@ def q8
 
   # 以下に回答を記載
   upper_case_programming_languages =
-    programming_languages.map(&:capitalize).map(&:upcase)
+    programming_languages.map(&:capitalize!).map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
